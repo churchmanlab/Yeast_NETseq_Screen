1f1fbe9da128364e524156096af4373dba927e1b
mtcouvi <62816886+mtcouvi@users.noreply.github.com>
2022-02-15 13:15:22 -0800

    Add files via upload

--- 5_PauseLoci/Scripts/calcPauseStrength.sh
+++ 5_PauseLoci/Scripts/calcPauseStrength.sh
@@ -32,19 +32,20 @@ do
 	genome=../0_Annotations/Scerevisiae_R64/SacCer3_R64_genomic.fasta.fai
 
     # Overlap and isolate genes that are above coverage threshold
-    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $posSignal | mapBed -a $posGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-1.covThreshold${t}.pos.bed
-    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $negSignal | mapBed -a $negGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-1.covThreshold${t}.neg.bed
+    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $posSignal | mapBed -a $posGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-1.ReadscovThreshold${t}.pos.bed
+    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $negSignal | mapBed -a $negGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-1.ReadscovThreshold${t}.neg.bed
 
     # Repeat for replicate 2
     posSignal=../1_AlignData/${mut}-2_stt_pos_sort.bedGraph 
     negSignal=../1_AlignData/${mut}-2_stt_neg_sort.bedGraph 
 
-    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $posSignal | mapBed -a $posGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-2.covThreshold${t}.pos.bed
-    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $negSignal | mapBed -a $negGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-2.covThreshold${t}.neg.bed
+    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $posSignal | mapBed -a $posGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-2.ReadsovThreshold${t}.pos.bed
+    awk -F "\t" 'BEGIN {OFS="\t"} ($4 > 0) {print $1, $2, $3, $4*($3-$2)}' $negSignal | mapBed -a $negGenes -g $genome -b - -c 4 -o sum | awk -F "\t" 'BEGIN {OFS="\t"} ($7 != ".") {print $0, $7/($3-$2)}' | awk -v t=$t -F "\t" 'BEGIN {OFS="\t"} ($8 > t) {print $1, $2, $3, $4, $5, $6, $7, $8}' > ${mut}-2.ReadscovThreshold${t}.neg.bed
 
     # Count number of reads in possible pauses for both replicates
-    reads1=`cat ${mut}-1.covThreshold${t}.???.bed | awk -F "\t" '{sum += $7} END {print sum}'`
-    reads2=`cat ${mut}-2.covThreshold${t}.???.bed | awk -F "\t" '{sum += $7} END {print sum}'`
+    reads1=`cat ${mut}-1.ReadscovThreshold${t}.???.bed | awk -F "\t" '{sum += $7} END {print sum}'`
+    reads2=`cat ${mut}-2.ReadscovThreshold${t}.???.bed | awk -F "\t" '{sum += $7} END {print sum}'`
+
 
     # Count number of reads in pauses for both replicates
     pauses1=`cat ${mut}_PASS_pauseScore_cov${t}.txt | awk -F "\t" '{sum += $4} END {print sum}'`
