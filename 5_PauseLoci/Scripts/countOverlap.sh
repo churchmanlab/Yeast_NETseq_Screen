44803240d98108cfdeab0034cade7f7994dde7bf
mtcouvi <62816886+mtcouvi@users.noreply.github.com>
2022-02-17 21:14:43 -0800

    Add files via upload

--- 5_PauseLoci/Scripts/countOverlap.sh
+++ 5_PauseLoci/Scripts/countOverlap.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+#SBATCH -c 1
+#SBATCH -t 0-01:00
+#SBATCH -p short
+#SBATCH --mem=10G
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=youremail@gmail.com
+###
+# Use: 
+# t=2
+# sbatch -o logs/5_countOverlap.log -e logs/5_countOverlap.err ./Scripts/countOverlap.sh deletionStrainsCovFiltered.txt $t
+# 
+t=$2
+
 while read mut
 do
 
     echo $mut
 
-    start=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ${mut}.IDRrepPausesAll.bed | intersectBed -a ../0_Annotations/Genes/geneStart15p.bed -b - -wb -s | wc -l`
-    mid=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ${mut}.IDRrepPausesAll.bed | intersectBed -a ../0_Annotations/Genes/geneMid.bed -b - -wb -s | wc -l`
-    end=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ${mut}.IDRrepPausesAll.bed | intersectBed -a ../0_Annotations/Genes/geneEnd85p.bed -b - -wb -s | wc -l`
+    start=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ${mut}.IDRrepPausesAll_cov${t}.bed | intersectBed -a ../0_Annotations/Genes/txptsStart15p.bed -b - -wb -s | wc -l`
+    mid=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ${mut}.IDRrepPausesAll_cov${t}.bed | intersectBed -a ../0_Annotations/Genes/txptsMid.bed -b - -wb -s | wc -l`
+    end=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ${mut}.IDRrepPausesAll_cov${t}.bed | intersectBed -a ../0_Annotations/Genes/txptsEnd85p.bed -b - -wb -s | wc -l`
 
     total=$(echo $start + $mid + $end  | bc)
 
@@ -16,9 +29,9 @@ do
 done < $1
 
 # Shuffled control
-start=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ALL.IDRrep.SHUFFLE.bed | intersectBed -a ../0_Annotations/Genes/geneStart15p.bed -b - -wb -s | wc -l`
-mid=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ALL.IDRrep.SHUFFLE.bed | intersectBed -a geneMid.bed -b - -wb -s | wc -l`
-end=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ALL.IDRrep.SHUFFLE.bed | intersectBed -a ../0_Annotations/Genes/geneEnd85p.bed -b - -wb -s | wc -l`
+start=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ALL.IDRrep.SHUFFLE.bed | intersectBed -a ../0_Annotations/Genes/txptsStart15p.bed -b - -wb -s | wc -l`
+mid=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ALL.IDRrep.SHUFFLE.bed | intersectBed -a ../0_Annotations/Genes/txptsMid.bed -b - -wb -s | wc -l`
+end=`awk -F "\t" 'BEGIN {OFS="\t"} {print $1, $2, $3, "pause"NR, ".", $4}' ALL.IDRrep.SHUFFLE.bed | intersectBed -a ../0_Annotations/Genes/txptsEnd85p.bed -b - -wb -s | wc -l`
 
 total=$(echo $start + $mid + $end  | bc)
 
